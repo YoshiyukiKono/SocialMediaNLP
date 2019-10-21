b6493d9557bfde305b3802cdb50eea050d00cc51
Yoshiyuki Kono <ykono@cloudera.com>
2019-10-21 02:13:48 +0000

    Update setup.R

--- setup.R
+++ setup.R
@@ -1,1 +1,5 @@
+# https://spark.rstudio.com/
+install.packages("sparklyr")
+# You can install the sparklyr package from CRAN as follows:
+# To upgrade to the latest version of sparklyr, run the following command and restart your r session:
 devtools::install_github("rstudio/sparklyr")
