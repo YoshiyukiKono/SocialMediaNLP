bc4d25df599b82ccd59e20abd2f151d53054123a
Yoshiyuki Kono <ykono@cloudera.com>
2019-10-21 02:44:20 +0000

    Update setup.R

--- setup.R
+++ setup.R
@@ -2,4 +2,5 @@
 install.packages("sparklyr")
 # You can install the sparklyr package from CRAN as follows:
 # To upgrade to the latest version of sparklyr, run the following command and restart your r session:
-devtools::install_github("rstudio/sparklyr")
+# When I tried to use the following line on 21th Oct, CML Workbench did not recognize inputs to the REPL line of the right pane.
+# devtools::install_github("rstudio/sparklyr")
